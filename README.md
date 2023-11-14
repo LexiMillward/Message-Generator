2add415a64bc3a266886432c71d4278a59146722
LexiMillward <lexi.millward1990@gmail.com>
2023-11-14 14:04:24 +0000

    completed program to create random Jedi & updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Message-Generator
+
+### Discription
+This is a programme to randomly create a Jedi rank, lightsaber type/color and give a planet for a diplomatic mission. 
+
+### Features
+Functions: 
+1. Random number generator 
+2. Generator function to select random choices and return a string paragraph. 
+
+### How to Use
+call console.log(JediGenerator())
---
+++ app.js
@@ -0,0 +1,49 @@
+// Returns a random number 
+
+const randNum = num => {
+    return Math.floor(Math.random() * num);
+};
+
+
+// Function to run 
+const jediGenerator = () => {
+    const choices = {
+        rank: ['Padawan', 'Knight', 'Master', 'Grand Master'],
+        lightsaberType: ['single-blade', 'double-blade', 'switch-blade', 'crossguard-blade'],
+        lightsaberColour: ['blue', 'green', 'yellow', 'white', 'purple'],
+        assignmentPlanet: ['Tatooine', 'Coruscant', 'Naboo', 'Alderaan', 'Kashyyyk'],
+    };
+    
+    // A place to store the randomly selected choices.
+    let generatedJedi = [];
+
+    for (let choice in choices) {
+        let choiceIndx = randNum(choices[choice].length);
+    
+    switch (choice) {
+        case "rank":
+            generatedJedi.push(`You have been granted the rank of ${choices[choice][choiceIndx]}.`);
+            break;
+        case "lightsaberType":
+            generatedJedi.push(`You weild a ${choices[choice][choiceIndx]} lightsaber,`);
+            break;
+        case "lightsaberColour":
+            generatedJedi.push(`with a ${choices[choice][choiceIndx]} kyber-crystal.`);
+            break;
+        case "assignmentPlanet": 
+            generatedJedi.push(`You are needed on ${choices[choice][choiceIndx]} for an urgent diplomatic peace keeping mission. May the force be with you!`)
+            break;
+        default:
+            generatedJedi.push("These are not the droids you are looking for.")
+            break;
+        }
+    }
+
+    let formatGeneratedJedi = generatedJedi.join("\n");
+    return formatGeneratedJedi;
+}
+
+console.log(jediGenerator());
+
+
+
